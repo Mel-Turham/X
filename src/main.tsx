d09912a1e22d9b63f1b723f00c8a48f90521ab17
Mel/ <tchoikuemel06@gmail.com>
2024-05-27 07:08:19 +0100

    setup the provider value input

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ValueProvider } from './Contexts/contextInput.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<ValueProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ValueProvider>
 	</React.StrictMode>,
 );
