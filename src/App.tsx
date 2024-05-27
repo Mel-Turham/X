5ce8175ac5c2a480e9efacec907e9e22838b004f
Mel/ <tchoikuemel06@gmail.com>
2024-05-27 23:46:57 +0100

    Setup all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import LayoutHome from './Layouts/LayoutHome';
-import { Feeds, BookMarks, Messages, Explore } from './Pages/index';
+import {
+	Feeds,
+	BookMarks,
+	Messages,
+	Explore,
+	Profile,
+	Lists,
+	More,
+	Notifications,
+} from './Pages/index';
 import { NextUIProvider } from '@nextui-org/react';
 const App = () => {
 	const navigate = useNavigate();
@@ -12,6 +21,10 @@ const App = () => {
 					<Route path='bookmarks' element={<BookMarks />} />
 					<Route path='messages' element={<Messages />} />
 					<Route path='explore' element={<Explore />} />
+					<Route path='Profile' element={<Profile />} />
+					<Route path='lists' element={<Lists />} />
+					<Route path='more' element={<More />} />
+					<Route path='notifications' element={<Notifications />} />
 				</Route>
 			</Routes>
 		</NextUIProvider>
