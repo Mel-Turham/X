7f7002be07fbdadfb5748f6d4c480a9412ac159c
Mel/ <tchoikuemel06@gmail.com>
2024-06-03 23:41:09 +0100

    Remove nextUi provide  to routes file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import LayoutHome from './Layouts/LayoutHome';
 import {
 	Feeds,
@@ -13,30 +13,30 @@ import {
 	Media,
 	Tweets,
 } from './Pages/index';
-import { NextUIProvider } from '@nextui-org/react';
+
+import { Login, Register } from './Auth/index';
 import LayoutProfile from './Layouts/LayoutProfile';
 const App = () => {
-	const navigate = useNavigate();
 	return (
-		<NextUIProvider navigate={navigate}>
-			<Routes>
-				<Route path='/' element={<LayoutHome />}>
-					<Route index element={<Feeds />} />
-					<Route path='bookmarks' element={<BookMarks />} />
-					<Route path='messages' element={<Messages />} />
-					<Route path='explore' element={<Explore />} />
-					<Route path='profile' element={<LayoutProfile />}>
-						<Route index element={<Tweets />} />
-						<Route path='likes' element={<Likes />} />
-						<Route path='media' element={<Media />} />
-						<Route path='tweetsReplies' element={<TweetsReplies />} />
-					</Route>
-					<Route path='lists' element={<Lists />} />
-					<Route path='more' element={<More />} />
-					<Route path='notifications' element={<Notifications />} />
+		<Routes>
+			<Route path='/' element={<LayoutHome />}>
+				<Route index element={<Feeds />} />
+				<Route path='bookmarks' element={<BookMarks />} />
+				<Route path='messages' element={<Messages />} />
+				<Route path='explore' element={<Explore />} />
+				<Route path='profile' element={<LayoutProfile />}>
+					<Route index element={<Tweets />} />
+					<Route path='likes' element={<Likes />} />
+					<Route path='media' element={<Media />} />
+					<Route path='tweetsReplies' element={<TweetsReplies />} />
 				</Route>
-			</Routes>
-		</NextUIProvider>
+				<Route path='lists' element={<Lists />} />
+				<Route path='more' element={<More />} />
+				<Route path='notifications' element={<Notifications />} />
+			</Route>
+			<Route path='login' element={<Login />} />
+			<Route path='register' element={<Register />} />
+		</Routes>
 	);
 };
 export default App;
